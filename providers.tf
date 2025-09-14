e0426aefc73b7be948236440ffbe8b80a985e607
nr1381 <nr1381.dev@gmail.com>
2025-09-14 12:42:34 +0900

    fix: snowflake provider uses JWT (key-pair) + username

--- providers.tf
+++ providers.tf
@@ -1,15 +1,3 @@
-variable "account" {}
-variable "user" {}
-variable "role" { default = "SECURITYADMIN" }
-variable "private_key_path" {}
-variable "private_key_passphrase" { default = null }
-
-provider "snowflake" {
-  account        = var.account
-  username       = var.user
-  role           = var.role
-  authenticator  = "SNOWFLAKE_JWT"
-  private_key    = file(var.private_key_path)
-  private_key_passphrase = var.private_key_passphrase
-}
+# providers.tf
+provider "snowflake" {}
 
