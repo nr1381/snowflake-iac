5006a627be90fba03f44feddfce6006a1cae281e
nr1381 <nr1381.dev@gmail.com>
2025-09-14 12:26:11 +0900

    fix: snowflake provider uses JWT (key-pair) + username

--- providers.tf
+++ providers.tf
@@ -2,13 +2,18 @@ variable "account" {}
 variable "user" {}
 variable "role" { default = "SECURITYADMIN" }
 variable "private_key_path" {}
+# ある場合だけ:
+# variable "private_key_passphrase" {}
 
 provider "snowflake" {
-  account          = var.account        # 例: xy12345.ap-northeast-1
-  user             = var.user           # 例: TF_ADMIN
-  role             = var.role
-  private_key_path = var.private_key_path
-  # パスフレーズ付きなら:
+  account       = var.account          # 例: RJ12293.ap-northeast-1
+  username      = var.user             # 例: TF_ADMIN
+  role          = var.role
+
+  # ← キーペア認証を使う宣言（これが無いと private_key_path が弾かれる）
+  authenticator     = "snowflake_jwt"
+
+  private_key_path  = var.private_key_path
   # private_key_passphrase = var.private_key_passphrase
 }
 
